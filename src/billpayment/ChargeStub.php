e774238d591fc9a79af0e07696bd8e82541eb0ae
Apiwat33 <33195886+apiwat33@users.noreply.github.com>
2017-11-04 10:19:13 +0700

    Finish Stub

--- src/billpayment/ChargeStub.php
+++ src/billpayment/ChargeStub.php
@@ -1,13 +1,79 @@
 <?php
-
-require_once 'DBConnection.php';
+namespace Operation;
 
 class ChargeStub {
 	
-    public static function getCharge( $accountNumber,  $serviceType )
-	{
-        return 0;
+	private $accountNumber,$chargeType;
+	
+	public function __construct(string $accountNumber,string $chargeType){
+        $this->accountNumber = $accountNumber;
+        $this->chargeType = $chargeType;
     }
+
+	public function getBill(){
+        $WATER_BILLING = 0;
+		$ELECTRIC_BILLING = 0;
+		$PHONE_BILLING = 0;
+		
+		if($this->accountNumber == '0123444667'){
+				$WATER_BILLING = 10000;
+				$ELECTRIC_BILLING = 53000;
+				//$ELECTRIC_BILLING = 0;
+				$PHONE_BILLING = 400000;
+		 }else if($this->accountNumber == '4235750021'){
+				$WATER_BILLING = 410000;
+				$ELECTRIC_BILLING = 3500000;
+				$PHONE_BILLING = 200000;
+		 }else if($this->accountNumber == '5902150431'){
+				$WATER_BILLING = 5004000;
+				$ELECTRIC_BILLING = 0;
+				$PHONE_BILLING = 1150250;
+		 }else if($this->accountNumber == '9835602413'){
+				$WATER_BILLING = 0;
+				$ELECTRIC_BILLING = 300000;
+				$PHONE_BILLING = 240000;
+		 }else if($this->accountNumber == '7840125312'){
+				$WATER_BILLING = 10000;
+				$ELECTRIC_BILLING = 10000;
+				$PHONE_BILLING = 0;
+		 }else if($this->accountNumber == '1924356780'){
+				$WATER_BILLING = 10000;
+				$ELECTRIC_BILLING = 700000;
+				$PHONE_BILLING = 100000;
+		 }else if($this->accountNumber == '2476492431'){
+				$WATER_BILLING = 100000;
+				$ELECTRIC_BILLING = 50000;
+				$PHONE_BILLING = 50000;
+		 }else if($this->accountNumber == '2476492431'){
+				$WATER_BILLING = 10000;
+				$ELECTRIC_BILLING = 5010;
+				$PHONE_BILLING = 5001;
+		 }else if($this->accountNumber == '2476492431'){
+				$WATER_BILLING = -1;
+				$ELECTRIC_BILLING = -30;
+				$PHONE_BILLING = -1000;
+		 }
+		 
+		 if($this->chargeType == 'WATER_BILLING'){
+			 
+			$chargeAmount = $WATER_BILLING;
+			
+		 }else if ($this->chargeType == 'ELECTRIC_BILLING'){
+			 
+			 $chargeAmount =  $ELECTRIC_BILLING;
+			 
+		 }else if ($this->chargeType == 'PHONE_BILLING'){
+			 
+			 $chargeAmount =  $PHONE_BILLING;
+			 
+		 }else{
+			 
+			$chargeAmount = 0 ;
+			
+		 }
+		
+		return $chargeAmount;
+   }
 }
 
 ?>
--- src/billpayment/ServiceAuthenticationStub.php
+++ src/billpayment/ServiceAuthenticationStub.php
@@ -1,24 +1,48 @@
 <?php
+namespace Operation;
 
-class ServiceAuthenticationStub
-{
-    public static function accountAuthenticationProvider( $accNo )
-	{
-		$mockUpAccountNumber=1234567890;
-		$mockUpAccountName="Pui";
-		$mockUpAccountBalance=500;
-		$mockUpErrorString="account not found";
-		$isError=false;
-		
-		if( !$isError )
-		{
-			return array('accNo'=>$accNo, 'accName'=>$mockUpAccountName, 'accBalance'=>$mockUpAccountBalance);
-		}
-		else
-		{
-			return AccountInformationException("Account number : {$accNo} not found.");
-		}
-    }    
+class ServiceAuthenticationStub {
+	
+	private $accountNumber;
+	
+	public function __construct(string $accountNumber){
+        $this->accountNumber = $accountNumber;
+    }
+
+         public function acauProvider(): array{ 
+			if ($this->accountNumber == '0123444667' ) {   
+					$answer = array('AccNo' => '0123444667','AccName'=> 'Kritsada Kancha','AccBalance'=> 2000000,'Message'=> null);
+
+				}else if($this->accountNumber == '4235850021') {
+					$answer = array('AccNo' => '4235850021','AccName'=> 'Tony Stark','AccBalance'=> 4000000,'Message'=> null);
+			
+				}else if($this->accountNumber == '5902150431') {
+					$answer = array('AccNo' => '5902150431','AccName'=> 'Bruce Wayne','AccBalance'=> 9555000,'Message'=> null);
+
+				}else if($this->accountNumber == '9835602413' ) {
+					$answer = array('AccNo' => '9835602413','AccName'=> 'Martin Flower','AccBalance'=> 400000,'Message'=> null);
+
+				}else if($this->accountNumber == '7840125312' ) {
+					$answer = array('AccNo' => '7840125312','AccName'=> 'SpearMan PicSun','AccBalance'=> 50001,'Message'=> null);
+
+				}else if($this->accountNumber == '1924356780' ) {
+					$answer = array('AccNo' => '1924356780','AccName'=> 'Johny Walker','AccBalance'=> 890200,'Message'=> null);
+
+				}else if($this->accountNumber == '2476492431' ) {
+					$answer = array('AccNo' => '2476492431','AccName'=> 'Jark Sparrow','AccBalance'=> 1000000,'Message'=> null);
+
+				}else if($this->accountNumber == '9178906629' ) {
+					$answer = array('AccNo' => '9178906629','AccName'=> 'Clara Oswald','AccBalance'=> 5000,'Message'=> null);
+
+				}else if($this->accountNumber == '3430368497' ) {
+					$answer = array('AccNo' => '3430368497','AccName'=> 'River Song','AccBalance'=> 10000,'Message'=> null);
+
+				}else {
+					$answer = array('AccNo' => '0000000000','AccName'=> 'xxxxxx','AccBalance'=> 0,'Message'=> 'Account Number not found');
+				}
+					return $answer;
+	}
+   
 }
 
 ?>
